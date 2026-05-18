010e89f255fd60a1aaa3302b7adc71a67e077e2d
jitsbenPC <jitsbenPC@gmail.com>
2026-05-18 00:07:35 -0500

    se creo la eestructura de carpetas de parte del administrador frontend

---
+++ src/app/features/dashboard/clinico/clinico.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clinico',
+  imports: [],
+  templateUrl: './clinico.html',
+  styleUrl: './clinico.css',
+})
+export class Clinico {}
---
+++ src/app/features/dashboard/facturacion/facturacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facturacion',
+  imports: [],
+  templateUrl: './facturacion.html',
+  styleUrl: './facturacion.css',
+})
+export class Facturacion {}
