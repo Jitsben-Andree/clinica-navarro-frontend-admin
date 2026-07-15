71d0e6f779c3605914ea1d992499d0cdbc47e93a
jitsbenPC <jitsbenPC@gmail.com>
2026-07-15 01:18:17 -0500

    backup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
+// 1. Importamos el nuevo interceptor anti-SSR
+import { ssrBlockInterceptor } from './core/interceptors/ssr-block.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    // 2. Colocamos ssrBlockInterceptor PRIMERO, y luego tu authInterceptor
+    provideHttpClient(withInterceptors([ssrBlockInterceptor, authInterceptor]))
   ]
 };
